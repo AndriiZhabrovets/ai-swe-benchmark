f9c6b247fdec0d524a5b743f7738c83e18f8c3e2
AndriiZhabrovets <zhabrovets.andrii@gmail.com>
2025-01-04 21:52:07 +0100

    putting everything to git

--- main/V2/v2.py
+++ main/V2/v2.py
@@ -5,17 +5,18 @@ import pandas as pd
 import json
 import subprocess
 import tempfile
-
+import psutil
 # Connecting the paths of all problems
 sys.path.insert(0, './main/problems/easy')
 sys.path.insert(0, './main/problems/medium')
 sys.path.insert(0, './main/problems/hard')
 
 class TestCase:
-    def __init__(self, status, error_msg, runtime):
+    def __init__(self, status, error_msg, runtime, memory):
         self.status = status
         self.error_msg = error_msg
         self.runtime = runtime
+        self.memory = memory
 
 easy_problems = [
     ["two_sum", "twoSum"],
@@ -44,24 +45,30 @@ def load_test_cases(filepath):
         data = json.load(file)
     return data
 
-def summary_print(st, dataset):
+def create_summary(st, dataset):
     for i, tcase in enumerate(st):
-        solved = "Success 🟢" if tcase.status else "Fail 🔴"
+        if tcase.status:
+            solved = "Success 🟢"
+        else:
+            solved = "Fail 🔴"
+
         msg = tcase.error_msg
         tm = tcase.runtime
 
         dataset["Test Case"].append(i)
         dataset["Status"].append(solved)
+        dataset["Memory Usage (mb)"].append(tcase.memory)
         dataset["Run Time (ms)"].append(tm)
 
-        print(f"\nTest Case {i}:")
-        print(f"Solution Status: {solved}")
-        print(f"Run Time: {round(tm, 3)} milliseconds")
         if not tcase.status:
-            print(f"Error Message: {msg}")
             dataset["Error Message"].append(msg)
         else:
             dataset["Error Message"].append(None)
+
+        # print(f"\nTest Case {i}:")
+        # print(f"Solution Status: {solved}")
+        # print(f"Run Time: {round(tm, 3)} milliseconds")
+        # print(f"Error Message: {msg}")
 
 def call_problem_subprocess(problem_name, args):
     with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
@@ -75,13 +82,16 @@ def call_problem_subprocess(problem_name, args):
         temp_file.flush()
 
         start_time = time.time()
+        process = psutil.Process(os.getpid())
         try:
             result = subprocess.check_output([sys.executable, temp_file.name], stderr=subprocess.PIPE, text=True).strip()
             runtime = (time.time() - start_time) * 1000
-            return json.loads(result), runtime, None
+            memory = process.memory_info().rss / 1000000
+            return json.loads(result), runtime, memory, None
         except subprocess.CalledProcessError as e:
             runtime = (time.time() - start_time) * 1000
-            return None, runtime, e.stderr
+            memory = process.memory_info().rss / 1000000
+            return None, runtime, memory, e.stderr
         finally:
             os.unlink(temp_file.name)
 
@@ -89,21 +99,17 @@ def solution_check(problem_name, cases_data):
     results = []
     test_cases = cases_data[problem_name[0]]
     for tcase in test_cases:
-        sol, runtime, error = call_problem_subprocess(problem_name, tcase["input"])
-        print(sol)
-        print(type(sol))
-        print(tcase["expected_output"])
-        print(type(tcase["expected_output"]))
+        sol, runtime, memory, error = call_problem_subprocess(problem_name, tcase["input"])
         if error:
-            results.append(TestCase(False, error, runtime))
+            results.append(TestCase(False, error, runtime, memory))
         elif sol in tcase["expected_output"]:
-            results.append(TestCase(True, None, runtime))
+            results.append(TestCase(True, None, runtime, memory))
         else:
-            results.append(TestCase(False, f"Incorrect output: {sol}", runtime))
+            results.append(TestCase(False, f"Incorrect output: {sol}", runtime, memory))
     return results
 
-def average_runtime(data):
-    return data["Run Time (ms)"].mean()
+def average_measure(data):
+    return [round(data["Run Time (ms)"].mean(),3), round(data["Memory Usage (mb)"].mean(),3)]
 
 def export_data(data, name):
     file_name = f'main/data/{name}.xlsx'
@@ -112,22 +118,32 @@ def export_data(data, name):
 
 def main():
     os.system("clear")
-    dataset = {
+    
+    total_dataset = {
+        'Problem': [],
+        'Average Run Time (ms)': [],
+        'Average Memory Usage (mb)': []
+    }
+
+    case_dataset = {
         'Test Case': [],
         'Status': [],
         'Run Time (ms)': [],
+        'Memory Usage (mb)': [],
         'Error Message': []
     }
+
     test_cases = load_test_cases('main/data/test_cases.json')
-    status = solution_check(easy_problems[0], test_cases)
-    summary_print(status, dataset)
+    status = solution_check(hard_problems[3], test_cases)
+    create_summary(status, case_dataset)
     print("\n")
 
-    solution_data = pd.DataFrame(dataset)
-    print(solution_data)
+    case_data = pd.DataFrame(case_dataset)
+    print(case_data)
     print("\n")
-    export_data(solution_data, 'TestData')
-    print(f"Average Run Time: {round(average_runtime(solution_data), 3)} milliseconds")
+    export_data(case_data, 'TestData')
+    print(f"Average Run Time: {average_measure(case_data)[0]} milliseconds")
+    print(f"Average Memory Usage: {average_measure(case_data)[1]} megabytes")
 
 if __name__ == "__main__":
     main()
